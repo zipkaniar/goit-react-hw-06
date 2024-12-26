45fd98bfe85e4996ab449d9ef44d813c713a6463
Murat Çolak <mo939614@gmail.com>
2024-12-27 01:36:19 +0300

    edit where bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./redux/store";
-import { store } from "./redux/store";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
