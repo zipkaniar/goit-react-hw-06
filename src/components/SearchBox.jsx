70c0daaf23ea024d2f408e9409ca380b77ae9328
Murat Çolak <mo939614@gmail.com>
2024-12-27 01:29:55 +0300

    edit

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { changeFilter } from '../redux/filtersSlice';
+import { changeFilter } from '../redux1/filtersSlice';
 
 
 const SearchBox = () => {
