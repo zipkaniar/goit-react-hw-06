793f5e4692f74f736fa9bcd3e4c182d62e406b59
Murat Çolak <mo939614@gmail.com>
2024-12-27 00:36:55 +0300

    last hit where

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from `react-router-dom`
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
-import { Provider } from `react-redux`;
+import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./Redux/store";
 import { store } from "./Redux/store";
